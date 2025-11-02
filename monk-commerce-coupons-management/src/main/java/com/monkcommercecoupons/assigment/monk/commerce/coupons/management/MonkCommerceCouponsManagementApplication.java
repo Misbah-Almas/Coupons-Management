40b3d89fe6f9fe8ae6eb431889a50c80e0cbc857
Misbah Almas <dev.misbahalmas@gmail.com>
2025-11-02 14:34:17 +0530

    initial commit

---
+++ monk-commerce-coupons-management/src/main/java/com/monkcommercecoupons/assigment/monk/commerce/coupons/management/MonkCommerceCouponsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.monkcommercecoupons.assigment.monk.commerce.coupons.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkCommerceCouponsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonkCommerceCouponsManagementApplication.class, args);
+	}
+
+}
---
+++ monk-commerce-coupons-management/src/test/java/com/monkcommercecoupons/assigment/monk/commerce/coupons/management/MonkCommerceCouponsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monkcommercecoupons.assigment.monk.commerce.coupons.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonkCommerceCouponsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
